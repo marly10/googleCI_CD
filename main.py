55c8031a7002e15affcccc3d22fbc6623e217d21
marly10 <mckmarly@icloud.com>
2021-01-25 17:44:26 -0800

    Update app to demonstrate new update on CI/CD

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('I update automatically!')
+        self.response.write('Hey ricky, I worked and I want to show this to you!!!!!')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
